b61c1e6bc40ed484ac2fe92a5b90d5f2bbdf3448
Matt Simons <matt@pi-top.com>
2017-03-16 12:58:44 +0000

    change path

--- webapp.go
+++ webapp.go
@@ -3,7 +3,7 @@ package main
 import "net/http"
 
 func main() {
-    fs := http.FileServer(http.Dir("images"))
+    fs := http.FileServer(http.Dir("/go/src/github.com/matt-simons/blue-green/images"))
     http.Handle("/", fs)
     panic(http.ListenAndServe(":8000", nil))
 }
