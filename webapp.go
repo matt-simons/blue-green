cebe6503d290ba17447a3312d3af2ddf8c90c302
Matt Simons <mattsimons@ntlworld.com>
2019-08-09 14:17:19 +0100

    Update webapp.go

--- webapp.go
+++ webapp.go
@@ -1,9 +1,46 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
 
 func main() {
-    fs := http.FileServer(http.Dir("/go/src/github.com/matt-simons/blue-green/static"))
-    http.Handle("/", fs)
-    panic(http.ListenAndServe(":8000", nil))
+	const tpl = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>{{.Region}}</title>
+	</head>
+	<body>
+		<h1>Hello {{.Region}}</h1>
+		<img src={{.Img}}></img>
+	</body>
+</html>`
+
+	check := func(err error) {
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	t, err := template.New("webpage").Parse(tpl)
+	check(err)
+
+	data := struct {
+		Region string
+		Img    string
+	}{
+		Region: os.Getenv("REGION"),
+		Img:    os.Getenv("IMG"),
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err = t.Execute(w, data)
+		check(err)
+	})
+
+	panic(http.ListenAndServe(":8000", nil))
 }
